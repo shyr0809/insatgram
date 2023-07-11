808d3ed1253d6d847e1ba202c944a5652d99f765
Jjol0twi <hgymin3129@gmail.com>
2023-07-11 12:24:18 +0900

    add dropdownlist to appbar

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,31 @@ class _MyHomePageState extends State<MyHomePage>
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white,
+        leadingWidth: MediaQuery.of(context).size.width * 0.5,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: DropdownButton(
+            underline: const SizedBox.shrink(),
+            alignment: Alignment.center,
+            items: dropDownItems.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(
+                  value,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 25),
+                ),
+              );
+            }).toList(),
+            value: dropDownItems[_selectedIndex],
+            onChanged: (String? value) {
+              setState(() {
+                _selecteddropDownItems = value!;
+                _selectedIndex = dropDownItems.indexOf(value);
+              });
+            },
+          ),
+        ),
       ),
             ),
             ),
