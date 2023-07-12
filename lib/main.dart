06cf7344aa56504324cca6f20ee261b42f81a74d
shyr0809 <shyr0809@gmail.com>
2023-07-12 20:43:53 +0900

    commit ddong

--- lib/main.dart
+++ lib/main.dart
@@ -9,26 +9,26 @@ import 'main_layout.dart';
 import 'pages/home_page.dart';
 
 void main() {
-runApp(
-MultiProvider(
-providers: [
-ChangeNotifierProvider(create: (context) => FeedService()),
-],
-child: const MyApp(),
-),
-);
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => FeedService()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-const MyApp({super.key});
+  const MyApp({super.key});
 
 // This widget is the root of your application.
-@override
-Widget build(BuildContext context) {
-return const MaterialApp(
-debugShowCheckedModeBanner: false,
-title: 'insatgram',
-home: MyHomePage(),
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'insatgram',
+      home: HomePage(),
+    );
+  }
 }
