6bd059251db5577acc7c382a7437985475a8cec1
kwonyongil <kkwonyi95@gmail.com>
2023-07-12 20:50:40 +0900

    리스트

---
+++ lib/pages/like_list.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+
+class LikeList {
+  final String image;
+  final String text;
+
+  LikeList({
+    required this.image,
+    required this.text,
+  });
+}
+
+List <LikeList> likelist = [
+  LikeList(
+    image: 'image', 
+    text: 'text',
+    ),
+    LikeList(
+    image: 'image', 
+    text: 'text',
+    ),
+    LikeList(
+    image: 'image', 
+    text: 'text',
+    ),
+    LikeList(
+    image: 'image', 
+    text: 'text',
+    ),
+    LikeList(
+    image: 'image', 
+    text: 'text',
+    ),
+]
