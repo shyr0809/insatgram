128db2912996b601f705e79faf4e6ad0d6123523
kwonyongil <kkwonyi95@gmail.com>
2023-07-12 20:50:40 +0900

    리스트

--- lib/pages/kwon_page.dart
+++ lib/pages/kwon_page.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
 
 class kwon_page extends StatelessWidget {
   const kwon_page({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('권용일님 페이지'),
-    );
+    return Scaffold();
   }
 }
