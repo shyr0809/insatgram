134ae3ac709c19406d6344002335d10cf3bd99fa
shyr0809 <shyr0809@gmail.com>
2023-07-12 14:49:48 +0900

    commit layout2

---
+++ lib/pages/kwon_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+class kwon_page extends StatelessWidget {
+  const kwon_page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('권용일님 페이지'),
+    );
+  }
+}
