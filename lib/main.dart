8bc12bb64215798bb8e96e98227bb3c483c7c66c
kwonyongil <kkwonyi95@gmail.com>
2023-07-13 10:18:27 +0900

    1차

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,32 @@
 import 'package:flutter/material.dart';
-import 'like_provider.dart';
-import 'home.dart';
 import 'package:insatgram/feed_service.dart';
 import 'package:provider/provider.dart';
 
 import 'main_layout.dart';
-
-import 'pages/home_page.dart';
+import 'pages/like_service.dart';
 
 void main() {
-runApp(
-MultiProvider(
-providers: [
-ChangeNotifierProvider(create: (context) => FeedService()),
-],
-child: const MyApp(),
-),
-);
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => FeedService()),
+        ChangeNotifierProvider(create: (context) => LikeList()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-const MyApp({super.key});
+  const MyApp({super.key});
 
 // This widget is the root of your application.
-@override
-Widget build(BuildContext context) {
-return const MaterialApp(
-debugShowCheckedModeBanner: false,
-title: 'insatgram',
-home: MyHomePage(),
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'insatgram',
+      home: MyHomePage(),
+    );
+  }
 }
