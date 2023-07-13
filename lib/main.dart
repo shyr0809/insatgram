03321436473feb032cd8e42e4dcf189e498f8952
shyr0809 <shyr0809@gmail.com>
2023-07-13 22:14:20 +0900

    .

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,13 @@ void main() {
       child: const MyApp(),
     ),
   );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'insatgram',
