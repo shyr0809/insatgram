7e98018b305020d1d58a4d08854d48d5d2f79b70
shyr0809 <shyr0809@gmail.com>
2023-07-13 14:59:36 +0900

    앱바 밑 스토리보드 부분 다트입니다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:insatgram/feed_service.dart';
+import 'package:insatgram/pages/home_page.dart';
 import 'package:provider/provider.dart';
-
-import 'MyHomePage.dart';
 
 void main() {
   runApp(
@@ -22,9 +21,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'insatgram',
-      home: MyHomePage(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'insatgram',
+        home: HomePage());
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:insatgram/pages/kwon_page.dart';
 
+import 'package:insatgram/pages/storyboardlist.dart';
+
+//15번째 줄부터 279번째 줄까지 앱바 밑 스크롤 뷰 위젯입니다.
+//286번째 줄부터 앱바입니다.
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -9,219 +13,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Widget _twelve() {
-    return InkWell(
-      onTap: () {},
-      child: Stack(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(65),
-            child: Container(
-                width: 60,
-                height: 60,
-                margin: const EdgeInsets.symmetric(horizontal: 1),
-                decoration:
-                    BoxDecoration(shape: BoxShape.circle, color: Colors.grey),
-                child: Image.network(
-                  "https://i.ibb.co/tKn3013/12jo.jpg",
-                  fit: BoxFit.cover,
-                )),
-          ),
-          Positioned(
-              right: 5,
-              bottom: 0,
-              child: Container(
-                width: 20,
-                height: 20,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.blue,
-                    border: Border.all(color: Colors.white, width: 2)),
-                child: const Center(
-                  child: Text(
-                    "+",
-                    style: TextStyle(
-                        fontSize: 15, color: Colors.white, height: 1.1),
-                  ),
-                ),
-              ))
-        ],
-      ),
-    );
-  }
-
-  Widget _kwon() {
-    return InkWell(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => kwon_page()),
-        );
-      },
-      child: Stack(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(65),
-            child: Container(
-              width: 60,
-              height: 60,
-              margin: const EdgeInsets.symmetric(horizontal: 1),
-              decoration:
-                  BoxDecoration(shape: BoxShape.circle, color: Colors.grey),
-              child: Image.network(
-                "https://i.ibb.co/RHBWbZ3/kwon.jpg",
-                fit: BoxFit.cover,
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget _lee() {
-    return InkWell(
-      onTap: () {},
-      child: Stack(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(65),
-            child: Container(
-                width: 60,
-                height: 60,
-                margin: const EdgeInsets.symmetric(horizontal: 1),
-                decoration:
-                    BoxDecoration(shape: BoxShape.circle, color: Colors.grey),
-                child: Image.network(
-                  "https://i.ibb.co/yNmwQH1/lee.jpg",
-                  fit: BoxFit.cover,
-                )),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget _jang() {
-    return InkWell(
-      onTap: () {},
-      child: Stack(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(65),
-            child: Container(
-                width: 60,
-                height: 60,
-                margin: const EdgeInsets.symmetric(horizontal: 1),
-                decoration:
-                    BoxDecoration(shape: BoxShape.circle, color: Colors.grey),
-                child: Image.network(
-                  "https://i.ibb.co/Z2y4hDF/jang.jpg",
-                  fit: BoxFit.cover,
-                )),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget _hwang() {
-    return InkWell(
-      onTap: () {},
-      child: Stack(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(65),
-            child: Container(
-                width: 60,
-                height: 60,
-                margin: const EdgeInsets.symmetric(horizontal: 1),
-                decoration:
-                    BoxDecoration(shape: BoxShape.circle, color: Colors.grey),
-                child: Image.network(
-                  "https://i.ibb.co/31ks7KK/hwang.jpg",
-                  fit: BoxFit.cover,
-                )),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget _hong() {
-    return InkWell(
-      onTap: () {},
-      child: Stack(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(65),
-            child: Container(
-                width: 60,
-                height: 60,
-                margin: const EdgeInsets.symmetric(horizontal: 1),
-                decoration:
-                    BoxDecoration(shape: BoxShape.circle, color: Colors.grey),
-                child: Image.network(
-                  "https://i.ibb.co/wdpPmYB/hong.jpg",
-                  fit: BoxFit.cover,
-                )),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget _storyBoardList() {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: (Row(children: [
-        const SizedBox(
-          width: 10,
-        ),
-        _twelve(),
-        const SizedBox(
-          width: 10,
-        ),
-        _kwon(),
-        const SizedBox(
-          width: 10,
-        ),
-        _lee(),
-        const SizedBox(
-          width: 10,
-        ),
-        _jang(),
-        const SizedBox(
-          width: 10,
-        ),
-        _hwang(),
-        const SizedBox(
-          width: 10,
-        ),
-        _hong(),
-        const SizedBox(
-          width: 10,
-        ),
-        ...List.generate(
-            1,
-            (index) => ClipRRect(
-                  borderRadius: BorderRadius.circular(60),
-                  child: Container(
-                    width: 65,
-                    height: 65,
-                    margin: const EdgeInsets.symmetric(horizontal: 10),
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle, color: Colors.grey),
-                    child: Image.network(
-                      "https://i.ibb.co/nngK6j3/startup.png",
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ))
-      ])),
-    );
-  }
-
   bool isFavorite = false;
 
   @override
@@ -237,7 +28,7 @@ class _HomePageState extends State<HomePage> {
           )),
       body: ListView(
         children: [
-          _storyBoardList(),
+          storyBoardList(),
           Divider(
             height: 5,
           ),
@@ -265,7 +56,7 @@ class _HomePageState extends State<HomePage> {
               });
             },
             child: Padding(
-              padding: const EdgeInsets.all(16),
+              padding: const EdgeInsets.all(0),
               child: Image.network(
                 "https://i.ibb.co/vz5Xw9D/12jjo.jpg",
                 height: 250,
@@ -274,6 +65,22 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
+          Row(
+            children: [
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isFavorite = !isFavorite;
+                    });
+                  },
+                  icon: Icon(
+                    isFavorite
+                        ? CupertinoIcons.heart_fill
+                        : CupertinoIcons.heart,
+                    color: isFavorite ? Colors.pink : Colors.black,
+                  )),
+            ],
+          )
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
